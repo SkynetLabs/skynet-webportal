a35d66a1cb3b02bf5ba2587d3f56634cb2574b9b
Robby Klein <hello@robbyk.com>
2020-02-12 14:13:34 -0800

    faster animations and copy button

--- packages/sia-skynet/src/components/HomeSamples/HomeSamples.js
+++ packages/sia-skynet/src/components/HomeSamples/HomeSamples.js
@@ -1,37 +1,42 @@
-import React from 'react'
-import Fade from 'react-reveal/Fade'
-import Reveal from 'react-reveal/Reveal'
+import React from "react";
+import Fade from "react-reveal/Fade";
+import Reveal from "react-reveal/Reveal";
 
-import './HomeSamples.scss'
-import { Sample } from '../'
+import "./HomeSamples.scss";
+import { Sample } from "../";
 
 const samples = [
-  { type: 'HTML', url: '#' },
-  { type: 'PDF', url: '#' },
-  { type: 'Image', url: '#' },
-  { type: 'Audio', url: '#' },
-  { type: 'Video', url: '#' },
-  { type: 'JSON', url: '#' },
-  { type: 'Dapps', url: '#' },
-]
+  { type: "HTML", url: "#" },
+  { type: "PDF", url: "#" },
+  { type: "Image", url: "#" },
+  { type: "Audio", url: "#" },
+  { type: "Video", url: "#" },
+  { type: "JSON", url: "#" },
+  { type: "Dapps", url: "#" }
+];
 
 export default function HomeSamples() {
   return (
     <div className="home-samples">
-      <Fade distance="40px" bottom>
+      <Fade duration={700} distance="40px" bottom>
         <p>
-          Skynet has SDKs for popular programming languages which integrate seamlessly with existing applications. Above
-          are a few code snippets for uploading and downloading data from Skynet.
+          Skynet has SDKs for popular programming languages which integrate
+          seamlessly with existing applications. Above are a few code snippets
+          for uploading and downloading data from Skynet.
         </p>
       </Fade>
 
       <Reveal effect="active">
         <div className="home-samples-samples">
           {samples.map((sample, i) => (
-            <Sample className={`fadeInUp delay${(i + 1) * 2}`} key={`${i}-${sample.url}`} {...sample} />
+            <Sample
+              className={`fadeInUp delay${i + 1}`}
+              key={`${i}-${sample.url}`}
+              {...sample}
+            />
           ))}
         </div>
       </Reveal>
     </div>
-  )
+  );
 }
--- packages/sia-skynet/src/components/HomeTop/HomeTop.js
+++ packages/sia-skynet/src/components/HomeTop/HomeTop.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import Reveal from 'react-reveal/Reveal'
+import React from "react";
+import Reveal from "react-reveal/Reveal";
 
-import './HomeTop.scss'
-import { Skynet, Deco1, Deco2 } from '../../svg'
+import "./HomeTop.scss";
+import { Skynet, Deco1, Deco2 } from "../../svg";
 
 export default function HomeTop() {
   return (
@@ -12,11 +12,14 @@ export default function HomeTop() {
 
         <h1 className="fadeInUp delay2">Build the Future Web.</h1>
 
-        <p className="fadeInUp delay4">Skynet is a decentralized file sharing and content distribution protocol.</p>
+        <p className="fadeInUp delay3">
+          Skynet is a decentralized file sharing and content distribution
+          protocol.
+        </p>
 
-        <Deco1 className="deco-1 fadeInUp delay8" />
-        <Deco2 className="deco-2 fadeInUp delay8" />
+        <Deco1 className="deco-1 fadeInUp delay6" />
+        <Deco2 className="deco-2 fadeInUp delay6" />
       </div>
     </Reveal>
-  )
+  );
 }
