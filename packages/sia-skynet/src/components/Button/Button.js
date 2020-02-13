bb67bdcfcc3b4fc214ac614ebe8382d87f3e231b
Robby Klein <hello@robbyk.com>
2020-02-13 14:07:42 -0800

    retrieve file

--- packages/sia-skynet/src/components/Button/Button.js
+++ packages/sia-skynet/src/components/Button/Button.js
@@ -1,28 +1,41 @@
-import React from 'react'
-import classNames from 'classnames'
+import React from "react";
+import classNames from "classnames";
 
-import './Button.scss'
+import "./Button.scss";
 
-export default function Button({ href, type, children, full, className, ...rest }) {
-  const classes = classNames('button', { full }, className)
+export default function Button({
+  href,
+  type,
+  children,
+  full,
+  className,
+  iconLeft,
+  iconRight,
+  ...rest
+}) {
+  const classes = classNames(
+    "button",
+    { iconLeft, iconRight, full },
+    className
+  );
 
   if (href) {
     return (
       <a href={href} className={classes} {...rest}>
         {children}
       </a>
-    )
+    );
   } else if (type) {
     return (
       <button type={type} className={classes} {...rest}>
         {children}
       </button>
-    )
+    );
   } else {
     return (
       <button type="button" className={classes} {...rest}>
         {children}
       </button>
-    )
+    );
   }
 }
