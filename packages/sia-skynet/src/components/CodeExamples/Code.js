51205ef9d99af38d1d951de02abc9875f6007086
Karol Wypchlo <kwypchlo@gmail.com>
2020-02-18 12:26:00 +0100

    reformat white spaces

--- packages/sia-skynet/src/components/CodeExamples/Code.js
+++ packages/sia-skynet/src/components/CodeExamples/Code.js
@@ -18,11 +18,11 @@ export const node = `const skynet = require('@nebulous/skynet');
 
 (async () => {
 	// upload
-    const skylink = await skynet.UploadFile(
+	const skylink = await skynet.UploadFile(
 		"./upload.jpg",
-        skynet.DefaultUploadOptions
-    );
-    console.log(\`Upload successful, skylink: \${skylink}\`);
+		skynet.DefaultUploadOptions
+	);
+	console.log(\`Upload successful, skylink: \${skylink}\`);
         
 	// download
 	await skynet.DownloadFile(
@@ -30,32 +30,32 @@ export const node = `const skynet = require('@nebulous/skynet');
 		skylink,
 		skynet.DefaultDownloadOptions
 	);
-    console.log('Download successful');
+	console.log('Download successful');
 })()`
 
 export const go = `package main
 
 import (
-    "fmt"
-    skynet "github.com/NebulousLabs/go-skynet"
+	"fmt"
+	skynet "github.com/NebulousLabs/go-skynet"
 )
                         
 func main() {
 	// upload
-    skylink, err := skynet.UploadFile("./upload.jpg", skynet.DefaultUploadOptions)
-    if err != nil {
-        fmt.Printf("Unable to upload: %v", err.Error())
-        return
-    }
-    fmt.Printf("Upload successful, skylink: %v", skylink)
+	skylink, err := skynet.UploadFile("./upload.jpg", skynet.DefaultUploadOptions)
+	if err != nil {
+		fmt.Printf("Unable to upload: %v", err.Error())
+		return
+	}
+	fmt.Printf("Upload successful, skylink: %v", skylink)
 
 	// download
-    err = skynet.DownloadFile("./download.jpg", skylink, skynet.DefaultDownloadOptions)
-    if err != nil {
-        fmt.Printf("Something went wrong, please try again.\\nError: %v", err.Error())
-        return
-    }
-    fmt.Println("Download successful")
+	err = skynet.DownloadFile("./download.jpg", skylink, skynet.DefaultDownloadOptions)
+	if err != nil {
+		fmt.Printf("Something went wrong, please try again.\\nError: %v", err.Error())
+		return
+	}
+	fmt.Println("Download successful")
 }`
 
 export const ruby = ``
