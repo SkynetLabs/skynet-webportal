74e198a34603f27ff4f447d981015f23519c5a11
Karol Wypchlo <kwypchlo@gmail.com>
2020-02-18 12:22:16 +0100

    snippets changes proposal

--- packages/sia-skynet/src/components/CodeExamples/Code.js
+++ packages/sia-skynet/src/components/CodeExamples/Code.js
@@ -1,35 +1,61 @@
 export const python = `from siaskynet import Skynet
-path = "./image.jpg"
-skylink = Skynet.UploadFile(path)
-print("Upload successful, skylink: " + skylink)`
 
-export const curl = `curl -X POST "https://siasky.net/api/skyfile/[uuid]" -F file=@image.jpg`
+# upload
+skylink = Skynet.UploadFile("./upload.jpg")
+print("Upload successful, skylink: " + skylink)
+
+# download
+Skynet.DownloadFile("./download.jpg", skylink)
+print("Download successful)`
+
+export const curl = `# upload
+curl -X POST "https://siasky.net/skynet/skyfile/[uuid]" -F file=@upload.jpg
+
+# download
+curl "https://siasky.net/[skylink]" -o download.jpg`
 
 export const node = `const skynet = require('@nebulous/skynet');
 
 (async () => {
-	const path = "./image.jpg"
-	const skylink = await skynet.UploadFile(
-		path,
-		skynet.DefaultUploadOptions
-	)
-	console.log(\`Upload successful, skylink: \${skylink}\`)
+	// upload
+    const skylink = await skynet.UploadFile(
+		"./upload.jpg",
+        skynet.DefaultUploadOptions
+    );
+    console.log(\`Upload successful, skylink: \${skylink}\`);
+        
+	// download
+	await skynet.DownloadFile(
+		"./download.jpg",
+		skylink,
+		skynet.DefaultDownloadOptions
+	);
+    console.log('Download successful');
 })()`
 
 export const go = `package main
 
 import (
-	"fmt"
-	skynet "github.com/NebulousLabs/go-skynet"
+    "fmt"
+    skynet "github.com/NebulousLabs/go-skynet"
 )
-						
+                        
 func main() {
-	skylink, err := skynet.UploadFile("./image.jpg", skynet.DefaultUploadOptions)
-	if err != nil {
-		fmt.Printf("Unable to upload: %v", err.Error())
-		return
-	}
-	fmt.Printf("Upload successful, skylink: %v", skylink)
+	// upload
+    skylink, err := skynet.UploadFile("./upload.jpg", skynet.DefaultUploadOptions)
+    if err != nil {
+        fmt.Printf("Unable to upload: %v", err.Error())
+        return
+    }
+    fmt.Printf("Upload successful, skylink: %v", skylink)
+
+	// download
+    err = skynet.DownloadFile("./download.jpg", skylink, skynet.DefaultDownloadOptions)
+    if err != nil {
+        fmt.Printf("Something went wrong, please try again.\\nError: %v", err.Error())
+        return
+    }
+    fmt.Println("Download successful")
 }`
 
 export const ruby = ``
