0e6a860ca0500f6bf146d4abbbfc0a6544baa955
Karol Wypchlo <kwypchlo@gmail.com>
2020-02-21 18:07:50 +0100

    include real stats from siastats.info

---
+++ src/components/HomeNetwork/useStats.js
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+
+export const AVAILABLE_STATS = {
+  ONLINE_HOSTS_COUNT: "onlineHostsCount",
+  STORAGE_USED_TB: "storageUsedTB",
+  NETWORK_CAPACITY_TB: "networkCapacityTB",
+  STORAGE_COST_USD: "storageCostUSD",
+  BANDWIDTH_DOWN_COST_USD: "bandwidthDownCostUSD"
+};
+
+export default function useStats() {
+  const [stats, setStats] = useState({
+    [AVAILABLE_STATS.ONLINE_HOSTS_COUNT]: null,
+    [AVAILABLE_STATS.STORAGE_USED_TB]: null,
+    [AVAILABLE_STATS.NETWORK_CAPACITY_TB]: null,
+    [AVAILABLE_STATS.STORAGE_COST_USD]: null,
+    [AVAILABLE_STATS.BANDWIDTH_DOWN_COST_USD]: null
+  });
+
+  useEffect(() => {
+    async function fetchData() {
+      const [bandwidth, storage, price] = await Promise.all([getBandwidthStats(), getStorageStats(), getPriceStats()]);
+
+      setStats({ ...bandwidth, ...storage, ...price });
+    }
+
+    fetchData();
+  }, [setStats]);
+
+  return stats;
+}
+
+async function getBandwidthStats() {
+  // { up: 76.09, down: 102.66, upusd: 0.23, downusd: 0.32 }
+  const response = await fetch("https://siastats.info/dbs/bandwidthpricesdb.json");
+  const data = await response.json();
+  const current = data[data.length - 1];
+
+  return {
+    [AVAILABLE_STATS.BANDWIDTH_DOWN_COST_USD]: current.downusd
+  };
+}
+
+async function getPriceStats() {
+  // { price: 504.59, newcontractformation: 30.79, usd: 1.55, sfperfees: 8.98 }
+  const response = await fetch("https://siastats.info/dbs/storagepricesdb.json");
+  const data = await response.json();
+  const current = data[data.length - 1];
+
+  return {
+    [AVAILABLE_STATS.STORAGE_COST_USD]: current.usd
+  };
+}
+
+async function getStorageStats() {
+  // { block_height: 247816, block_timestamp: 1582285828, hashrate: 6212581269715416,
+  // difficulty: 3501953420754597000, coin_supply: 43638591164, coin_price_USD: 0.003,
+  // market_cap_USD: 130915773, used_storage_TB: 725.26, network_capacity_TB: 2270.96,
+  // online_hosts: 360, active_contracts: 62997 }
+  const response = await fetch("https://siastats.info/dbs/network_status.json");
+  const data = await response.json();
+
+  return {
+    [AVAILABLE_STATS.ONLINE_HOSTS_COUNT]: data.online_hosts,
+    [AVAILABLE_STATS.STORAGE_USED_TB]: data.used_storage_TB,
+    [AVAILABLE_STATS.NETWORK_CAPACITY_TB]: data.network_capacity_TB
+  };
+}
