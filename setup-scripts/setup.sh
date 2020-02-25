1e72f2ade025a1558e889ca461f311b8e810b1bd
Marcin Jachymiak <marcin.jachymiak1@gmail.com>
2020-02-25 12:52:04 -0500

    Add systemd service conf. Update setup script

--- setup-scripts/setup.sh
+++ setup-scripts/setup.sh
@@ -46,7 +46,7 @@ sudo ufw allow 'Nginx HTTP'
 # Install Go 1.13.7.
 wget -c https://dl.google.com/go/go1.13.7.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.13.7.linux-amd64.tar.gz
-source ~/.bashrc
+export PATH=$PATH:$HOME/go/bin
 rm go1.13.7.linux-amd64.tar.gz
 
 # Sanity check that will pass if go was installed correctly.
@@ -57,10 +57,17 @@ cwd=$(pwd)
 # Install Sia
 cd ~/
 git clone https://gitlab.com/NebulousLabs/Sia
-cd Sia && git checkout viewnode && make
+cd Sia && git checkout v1.4.3 && make
 
 # Setup skynet frontend.
 cd $cwd
 cd ../
 yarn
 yarn build
+
+# Enable the systemd service
+cd $cwd
+mkdir -p $HOME/.config/systemd/user
+cp siad.service $HOME/.config/systemd/user/siad.service
+
+systemctl --user enable siad.service
