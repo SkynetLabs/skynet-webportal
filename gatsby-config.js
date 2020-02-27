954a14758fc1a76ae12c0551dea3a9c0bdd18cd9
Karol Wypchlo <kwypchlo@gmail.com>
2020-02-27 11:58:53 +0100

    remove service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,6 @@ module.exports = {
         icon: `src/images/logo.svg` // This path is relative to the root of the site.
       }
     },
-    `gatsby-plugin-offline`
+    `gatsby-plugin-remove-serviceworker`
   ]
 };
