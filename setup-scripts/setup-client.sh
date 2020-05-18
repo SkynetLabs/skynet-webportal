55dff5ce1c50f8cea890c8a69d0cd69006a2fec2
Karol Wypchlo <kwypchlo@gmail.com>
2020-05-18 20:43:20 +0200

    change ubuntu to debian

--- setup-scripts/setup-client.sh
+++ setup-scripts/setup-client.sh
@@ -7,7 +7,7 @@ sudo apt-get remove -y docker docker-engine docker.io containerd runc
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 docker --version # sanity check
