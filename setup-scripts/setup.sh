3e21ed5a3a578607eb80893823a97942a39b236f
Marcin Jachymiak <marcin.jachymiak1@gmail.com>
2020-05-19 16:12:50 -0400

    Bump version numbers

--- setup-scripts/setup.sh
+++ setup-scripts/setup.sh
@@ -43,17 +43,17 @@ sudo ufw allow ssh
 sudo ufw allow 'Nginx Full'
 sudo ufw allow 'Nginx HTTP'
 
-# Install Go 1.13.7.
-wget -c https://dl.google.com/go/go1.13.7.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.13.7.linux-amd64.tar.gz
+# Install Go 1.13.11.
+wget -c https://dl.google.com/go/go1.13.11.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.13.11.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
-rm go1.13.7.linux-amd64.tar.gz
+rm go1.13.11.linux-amd64.tar.gz
 
 # Sanity check that will pass if go was installed correctly.
 go version
 
 # Install Sia
-git clone -b v1.4.4 https://gitlab.com/NebulousLabs/Sia ~/Sia
+git clone -b v1.4.8 https://gitlab.com/NebulousLabs/Sia ~/Sia
 make --directory ~/Sia
 
 # Setup systemd files
