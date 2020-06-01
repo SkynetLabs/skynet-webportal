af0e639e6dc2758d6a48aa5b7ff06212be6b2243
Karol Wypchlo <kwypchlo@gmail.com>
2020-06-01 11:22:06 +0200

    silence ssh

--- setup-scripts/blacklist.sh
+++ setup-scripts/blacklist.sh
@@ -8,8 +8,8 @@ fi
 
 for server in "germany.siasky.net" "us-east.siasky.net" "us-west.siasky.net" "helsinki.siasky.net" "siasky.dev";
 do
-    echo "⌁ Connecting to ${server}"
-    ssh -t user@${server} 'curl -A Sia-Agent --user "":$(cat ~/.sia/apipassword) --data '"'"'{"add":["'$1'"]}'"'"' "localhost:9980/skynet/blacklist"'
+    echo "⌁ Blacklisting on ${server}"
+    ssh -q -t user@${server} 'curl -A Sia-Agent --user "":$(cat ~/.sia/apipassword) --data '"'"'{"add":["'$1'"]}'"'"' "localhost:9980/skynet/blacklist"'
 done
 
 echo "✓ All portals succesfully blacklisted provided skylink"
