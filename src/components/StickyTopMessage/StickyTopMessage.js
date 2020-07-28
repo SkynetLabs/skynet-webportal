dd0f40b945910ea6cf41ba953ad1b6681a38c4be
Karol Wypchlo <kwypchlo@gmail.com>
2020-07-28 14:03:08 +0200

    drop 1.5.0 message

--- src/components/StickyTopMessage/StickyTopMessage.js
+++ src/components/StickyTopMessage/StickyTopMessage.js
@@ -1,13 +1,14 @@
-import React from "react";
-import "./StickyTopMessage.scss";
+// import React from "react";
+// import "./StickyTopMessage.scss";
 
 export default function StickyTopMessage() {
-  return (
-    <div className="sticky-top-message" role="alert">
-      <p>
-        We are working on improved performance as part of the upcoming Sia v1.5.0 release. In the meantime, you can try
-        other webportals listed at the <a href="#footer">bottom</a>.
-      </p>
-    </div>
-  );
+  return null; // returns nothing, uncomment when we need a new message
+  // return (
+  //   <div className="sticky-top-message" role="alert">
+  //     <p>
+  //       We are working on improved performance as part of the upcoming Sia v1.5.0 release. In the meantime, you can try
+  //       other webportals listed at the <a href="#footer">bottom</a>.
+  //     </p>
+  //   </div>
+  // );
 }
