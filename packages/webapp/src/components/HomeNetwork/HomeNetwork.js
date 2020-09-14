0403a5a8da28e4b5d6f7eaefe8cc7906ec5484ef
Karol Wypchlo <kwypchlo@gmail.com>
2020-09-14 11:20:51 +0200

    remove reveal and fade effects

--- packages/webapp/src/components/HomeNetwork/HomeNetwork.js
+++ packages/webapp/src/components/HomeNetwork/HomeNetwork.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import CountUp from "react-countup";
 import VisibilitySensor from "react-visibility-sensor";
-import Fade from "react-reveal/Fade";
 
 import useStats, { AVAILABLE_STATS } from "./useStats";
 import "./HomeNetwork.scss";
@@ -29,147 +28,126 @@ export default function HomeNetwork() {
   return (
     <div className="home-network">
       <header className="home-network-header">
-        <Fade duration={700} distance="40px" bottom>
-          <div className="home-network-divider">
-            <CircleIcon>
-              <LogoSolid />
-            </CircleIcon>
-            <SmallOrb />
-            <div className="divider"></div>
-            <div className="small-divider"></div>
-          </div>
-        </Fade>
-        <Fade duration={700} distance="40px" bottom>
-          <h2>
-            Sia
-            <br />
-            <strong>Network</strong>
-          </h2>
-        </Fade>
+        <div className="home-network-divider">
+          <CircleIcon>
+            <LogoSolid />
+          </CircleIcon>
+          <SmallOrb />
+          <div className="divider"></div>
+          <div className="small-divider"></div>
+        </div>
+        <h2>
+          Sia
+          <br />
+          <strong>Network</strong>
+        </h2>
       </header>
 
-      <Fade duration={700} distance="40px" bottom>
-        <VisibilitySensor onChange={onChange} partialVisibility offset={{ bottom: 100 }} scrollThrottle={50}>
-          <React.Fragment>
-            <div className="home-network-stats">
-              {STATS_MAP.map((stat, i) => (
-                <React.Fragment key={i}>
-                  <div key={i} className="home-network-stat">
-                    <div className="inner">
-                      {visible && <StatValue stat={stat} value={stats[stat.key]} />}
-                      <span className="network-stat-name">{stat.name}</span>
-                    </div>
+      <VisibilitySensor onChange={onChange} partialVisibility offset={{ bottom: 100 }} scrollThrottle={50}>
+        <React.Fragment>
+          <div className="home-network-stats">
+            {STATS_MAP.map((stat, i) => (
+              <React.Fragment key={i}>
+                <div key={i} className="home-network-stat">
+                  <div className="inner">
+                    {visible && <StatValue stat={stat} value={stats[stat.key]} />}
+                    <span className="network-stat-name">{stat.name}</span>
                   </div>
-                  {i !== 4 && <div className="divider" />}
-                </React.Fragment>
-              ))}
-              <Deco6 className="deco-6" />
-              <Deco7 className="deco-7" />
-              <Deco8 className="deco-8" />
-            </div>
-            <div className="home-network-stats-provider">
-              stats provided by{" "}
-              <a href="https://siastats.info" target="_blank" rel="noopener noreferrer">
-                siastats.info
-              </a>
-            </div>
-          </React.Fragment>
-        </VisibilitySensor>
-      </Fade>
+                </div>
+                {i !== 4 && <div className="divider" />}
+              </React.Fragment>
+            ))}
+            <Deco6 className="deco-6" />
+            <Deco7 className="deco-7" />
+            <Deco8 className="deco-8" />
+          </div>
+          <div className="home-network-stats-provider">
+            stats provided by{" "}
+            <a href="https://siastats.info" target="_blank" rel="noopener noreferrer">
+              siastats.info
+            </a>
+          </div>
+        </React.Fragment>
+      </VisibilitySensor>
 
       <div className="home-network-columns">
         <div className="home-network-column left">
-          <Fade duration={700} distance="40px" bottom>
-            <p>
-              <strong>Skynet Webportals</strong> are low cost servers that sit between Skynet and everyday users,
-              enabling them to access Skynet content without needing to operate any special software. As Skylinks are
-              generated, they can be shared with anyone to retrieve data from any Webportal. The original uploader does
-              not need to stay online in order for the file to remain available. The Sia network serves as the backend
-              storage layer for Skynet and handles all of the pinning, guaranteeing both high speeds and excellent
-              uptime.
-            </p>
-          </Fade>
+          <p>
+            <strong>Skynet Webportals</strong> are low cost servers that sit between Skynet and everyday users, enabling
+            them to access Skynet content without needing to operate any special software. As Skylinks are generated,
+            they can be shared with anyone to retrieve data from any Webportal. The original uploader does not need to
+            stay online in order for the file to remain available. The Sia network serves as the backend storage layer
+            for Skynet and handles all of the pinning, guaranteeing both high speeds and excellent uptime.
+          </p>
 
-          <Fade duration={700} distance="40px" bottom>
-            <p>
-              <strong>Sia</strong> is the leading decentralized cloud storage platform. No signups, no servers, no
-              trusted third parties. Sia leverages blockchain technology to create a data storage marketplace that is
-              more robust and more affordable than traditional cloud storage providers.
-            </p>
+          <p>
+            <strong>Sia</strong> is the leading decentralized cloud storage platform. No signups, no servers, no trusted
+            third parties. Sia leverages blockchain technology to create a data storage marketplace that is more robust
+            and more affordable than traditional cloud storage providers.
+          </p>
 
-            <p>
-              <a className="more" href="https://sia.tech/" target="_blank" rel="noopener noreferrer">
-                Learn more about Sia
-              </a>
-            </p>
-          </Fade>
+          <p>
+            <a className="more" href="https://sia.tech/" target="_blank" rel="noopener noreferrer">
+              Learn more about Sia
+            </a>
+          </p>
         </div>
         <div className="home-network-column">
-          <Fade duration={700} distance="40px" bottom>
-            <FAQ title="How do I use Skynet?">
-              <p>
-                Anyone can access files that have been uploaded to Skynet as long as they possess the corresponding
-                Skylinks. You can use any Webportal to download files!
-                <a
-                  href="https://skynet.helpdocs.io/article/3p9z5g9s0e-skynet-how-to"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="more read-more"
-                >
-                  read more
-                </a>
-              </p>
-            </FAQ>
-          </Fade>
+          <FAQ title="How do I use Skynet?">
+            <p>
+              Anyone can access files that have been uploaded to Skynet as long as they possess the corresponding
+              Skylinks. You can use any Webportal to download files!
+              <a
+                href="https://skynet.helpdocs.io/article/3p9z5g9s0e-skynet-how-to"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="more read-more"
+              >
+                read more
+              </a>
+            </p>
+          </FAQ>
 
-          <Fade duration={700} distance="40px" bottom>
-            <FAQ title="How do I integrate Skynet into my app?">
-              <p>
-                Applications can be deployed in under a minute and be immediately available globally. Skynet includes an
-                API and SDKs which integrate seamlessly with existing applications.
-                <a
-                  href="https://skynet.helpdocs.io/article/hrshqsn9wz-integrating-skynet"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="more read-more"
-                >
-                  read more
-                </a>
-              </p>
-            </FAQ>
-          </Fade>
+          <FAQ title="How do I integrate Skynet into my app?">
+            <p>
+              Applications can be deployed in under a minute and be immediately available globally. Skynet includes an
+              API and SDKs which integrate seamlessly with existing applications.
+              <a
+                href="https://skynet.helpdocs.io/article/hrshqsn9wz-integrating-skynet"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="more read-more"
+              >
+                read more
+              </a>
+            </p>
+          </FAQ>
 
-          <Fade duration={700} distance="40px" bottom>
-            <FAQ title="How fast is Skynet?">
-              <p>
-                Skynet&apos;s speeds rival centralized providers and surpass all decentralized offerings. A typical
-                Skynet download starts in under 500 ms and can stream at rates as high as 1 Gbps!
-                <a
-                  href="https://skynet.helpdocs.io/article/430teoxgqc-skynet-speed"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="more read-more"
-                >
-                  read more
-                </a>
-              </p>
-            </FAQ>
-          </Fade>
+          <FAQ title="How fast is Skynet?">
+            <p>
+              Skynet&apos;s speeds rival centralized providers and surpass all decentralized offerings. A typical Skynet
+              download starts in under 500 ms and can stream at rates as high as 1 Gbps!
+              <a
+                href="https://skynet.helpdocs.io/article/430teoxgqc-skynet-speed"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="more read-more"
+              >
+                read more
+              </a>
+            </p>
+          </FAQ>
 
-          <Fade duration={700} distance="40px" bottom>
-            <FAQ title="How much does it cost to run a Webportal?">
-              <p>
-                Storage costs 10x lower than centralized providers and bandwidth costs are 100x lower – without
-                sacrificing performance or reliability.
-              </p>
-            </FAQ>
-          </Fade>
+          <FAQ title="How much does it cost to run a Webportal?">
+            <p>
+              Storage costs 10x lower than centralized providers and bandwidth costs are 100x lower – without
+              sacrificing performance or reliability.
+            </p>
+          </FAQ>
 
-          <Fade duration={700} distance="40px" bottom>
-            <a className="more more-faq" href="https://support.siasky.net" target="_blank" rel="noopener noreferrer">
-              View more FAQ
-            </a>
-          </Fade>
+          <a className="more more-faq" href="https://support.siasky.net" target="_blank" rel="noopener noreferrer">
+            View more FAQ
+          </a>
         </div>
       </div>
     </div>
