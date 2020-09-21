c03c482d3650984dc8ec6d25bc85485b10d3b52c
Karol Wypchlo <kwypchlo@gmail.com>
2020-09-21 18:48:46 +0200

    update portal upgrade script

--- scripts/portal-upgrade.sh
+++ scripts/portal-upgrade.sh
@@ -8,9 +8,18 @@ cwd=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 # put the server down for maintenance
 . ${cwd}/portal-down.sh
 
-# rebuild and restart all docker containers
-docker-compose build --no-cache
+# stop the docker services
 docker-compose down
+
+# clear unused docker containers so we don't run into out of disk space
+# it should be done after the container have been stopped and before 
+# building them again
+docker system prune --force
+
+# build all container without cache
+docker-compose build --no-cache
+
+# start the docker services
 docker-compose up -d
 
 # enable the server again
