c7cfff728d1553e4a7833b2021bdc5c26e43449e
Karol Wypchlo <kwypchlo@gmail.com>
2020-11-18 16:08:03 +0100

    comment out uniswap links from health-check

--- scripts/blocklist-skylink.sh
+++ scripts/blocklist-skylink.sh
@@ -6,9 +6,9 @@ if [ -z "$1" ]; then
     echo "Please provide a skylink to blocklist" && exit 1
 fi
 
-for server in "germany.siasky.net" "us-east.siasky.net" "us-west.siasky.net" "helsinki.siasky.net" "us-va-1.siasky.net" "us-pa-2.siasky.net";
+for server in "germany.siasky.net" "helsinki.siasky.net" "us-west.siasky.net" "us-va-1.siasky.net" "us-pa-1.siasky.net" "us-pa-2.siasky.net" "siasky.xyz";
 do
-    echo "⌁ Blacklisting on ${server}"
+    echo "⌁ Blocking skylink on ${server}"
     ssh -q -t user@${server} 'docker exec sia siac skynet blocklist add '$1''
     ssh -q -t user@${server} 'rm -rf /home/user/skynet_webportal/docker/data/nginx/cache' # prune nginx cache
 done
