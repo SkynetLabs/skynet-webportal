5f76d1ca52e3a80dc12345bfb82aca12f898f007
Matthew Sevey <mjsevey@gmail.com>
2020-11-24 07:49:51 -0700

    remove error alert notification, subtract out siafile alerts

--- setup-scripts/health-checker.py
+++ setup-scripts/health-checker.py
@@ -290,8 +290,10 @@ async def check_alerts():
         force_notify = True
     if num_error_alerts > 0:
         message += "{} Error Alerts found! ".format(num_error_alerts)
-        force_notify = num_error_alerts > num_siafile_alerts
 
+    # Subtract out the siafile alerts from the warning alerts since we announce
+    # them separately
+    num_warning_alerts -= num_siafile_alerts
     message += "{} Warning Alerts found. ".format(num_warning_alerts)
     message += "{} SiaFiles with bad health found. ".format(num_siafile_alerts)
 
