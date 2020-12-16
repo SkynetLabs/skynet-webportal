c806129ba763c86c3b544f7213a990804a7b21c6
Karol Wypchlo <kwypchlo@gmail.com>
2020-12-16 14:42:39 +0100

    rewrite /secure

--- packages/webapp/src/components/HomeTop/HomeTop.js
+++ packages/webapp/src/components/HomeTop/HomeTop.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Cookies from "js-cookie";
 import logo from "../../images/logo.svg";
 import "./HomeTop.scss";
 import { Skynet, Deco1, Deco2 } from "../../svg";
 
 export default function HomeTop() {
+  const authenticated = Boolean(Cookies.get("skynet-jwt"));
+
   return (
     <div className="home-top">
       <img src={logo} alt="Skynet logo" className="logo" />
@@ -16,13 +19,24 @@ export default function HomeTop() {
       </p>
 
       <p className="auth-links">
-        <a href="/secure/auth/registration" className="link">
-          Sign up now!
-        </a>{" "}
-        Already have an account?{" "}
-        <a href="/secure/auth/login" className="link">
-          Log in!
-        </a>
+        {authenticated ? (
+          <>
+            Welcome back! Go to your{" "}
+            <a href="/secure/" className="link">
+              dashboard
+            </a>
+          </>
+        ) : (
+          <>
+            <a href="/secure/auth/registration" className="link">
+              Sign up now!
+            </a>{" "}
+            Already have an account?{" "}
+            <a href="/secure/auth/login" className="link">
+              Log in!
+            </a>
+          </>
+        )}
       </p>
 
       <Deco1 className="deco-1" />
