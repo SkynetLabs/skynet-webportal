8e0160bfb7fcdf4f5c6f0aac74f8b9a84b31e8fb
Karol Wypchlo <kwypchlo@gmail.com>
2021-01-26 13:35:49 +0100

    remove problematic sharp dependency

--- packages/webapp/gatsby-config.js
+++ packages/webapp/gatsby-config.js
@@ -18,8 +18,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     `gatsby-plugin-robots-txt`,
     {
       resolve: `gatsby-plugin-manifest`,
