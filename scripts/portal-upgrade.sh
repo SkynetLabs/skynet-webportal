7904f42f79eda7b42a5d9a6cab4264abe9aeb648
Karol Wypchlo <kwypchlo@gmail.com>
2021-02-01 21:29:30 +0100

    build first, then restart

--- scripts/portal-upgrade.sh
+++ scripts/portal-upgrade.sh
@@ -8,6 +8,9 @@ cwd=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 # put the server down for maintenance
 . ${cwd}/portal-down.sh
 
+# build all container without cache
+docker-compose build --no-cache --parallel --pull --quiet
+
 # stop the docker services
 docker-compose down
 
@@ -19,9 +22,6 @@ docker system prune --force
 # prune all volumes to make sure there is no leftovers
 # important for webapp container, otherwise html files will not be updated)
 docker volume rm $(docker volume ls -q)
-
-# build all container without cache
-docker-compose build --no-cache --parallel --pull --quiet
 
 # start the docker services
 docker-compose up -d
