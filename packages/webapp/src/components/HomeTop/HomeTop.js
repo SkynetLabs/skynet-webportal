406ec7f428651399f253a76dfa6b7b598f3e8fb2
Karol Wypchlo <kwypchlo@gmail.com>
2021-02-01 21:05:22 +0100

    separate accounts from default docker-compose

--- packages/webapp/src/components/HomeTop/HomeTop.js
+++ packages/webapp/src/components/HomeTop/HomeTop.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { boolean } from "boolean";
 import logo from "../../images/logo.svg";
 import "./HomeTop.scss";
 import { Skynet, Deco1, Deco2 } from "../../svg";
@@ -15,15 +16,17 @@ export default function HomeTop() {
         The decentralized CDN and file sharing platform for devs. Skynet is the storage foundation for a Free Internet!
       </p>
 
-      <p className="auth-links">
-        <a href="/secure/auth/registration" className="link">
-          Sign up now!
-        </a>{" "}
-        Already have an account? Go to your{" "}
-        <a href="/secure/" className="link">
-          dashboard
-        </a>
-      </p>
+      {boolean(process.env.GATSBY_WITH_ACCOUNTS) && (
+        <p className="auth-links">
+          <a href="/secure/auth/registration" className="link">
+            Sign up now!
+          </a>{" "}
+          Already have an account? Go to your{" "}
+          <a href="/secure/" className="link">
+            dashboard
+          </a>
+        </p>
+      )}
 
       <Deco1 className="deco-1" />
       <Deco2 className="deco-2" />
