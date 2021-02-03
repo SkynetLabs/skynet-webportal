8afc5c3b1934b7954a451972376fed26e79026c1
Karol Wypchlo <kwypchlo@gmail.com>
2021-02-03 15:21:30 +0100

    fix prod build

--- packages/dashboard/tailwind.config.js
+++ packages/dashboard/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  purge: ["./pages/**/*.js", "./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
