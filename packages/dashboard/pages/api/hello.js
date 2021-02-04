a40a4f2ecb14c5bde878596a7edaf3a2d38190d9
Karol Wypchlo <kwypchlo@gmail.com>
2021-02-04 14:31:26 +0100

    test api

--- packages/dashboard/pages/api/hello.js
+++ packages/dashboard/pages/api/hello.js
@@ -1,6 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+import jwt from "express-jwt";
+import jwks from "jwks-rsa";
+import config from "../../src/config";
+
+// This middleware assumes that the app is secured using ORY Oathkeeper, in which case we
+// verify the JSON Web Token issued by ORY Oathkeeper using the jwt-express middleware.
+
+const middleware = jwt({
+  // Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint.
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: config.jwksUrl,
+  }),
+  algorithms: ["RS256"],
+});
+
+export default middleware((req, res) => {
+  console.log(Object.keys(req));
+  console.log(req);
+  console.log(JSON.stringify(req));
   res.statusCode = 200;
   res.json({ name: "John Doe" });
-};
+});
