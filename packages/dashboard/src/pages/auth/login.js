59eb9f4a89fd1a3c5c97452799d70d2be037ef3f
Karol Wypchlo <kwypchlo@gmail.com>
2021-02-09 15:25:01 +0100

    drop flow logging

--- packages/dashboard/src/pages/auth/login.js
+++ packages/dashboard/src/pages/auth/login.js
@@ -69,8 +69,6 @@ export default function Login({ flow }) {
     initialValues: fields.reduce((acc, field) => ({ ...acc, [field.name]: field.value }), {}),
   });
 
-  console.log("flow", flow);
-
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
--- packages/dashboard/src/pages/recovery.js
+++ packages/dashboard/src/pages/recovery.js
@@ -56,7 +56,6 @@ export default function Recovery({ flow }) {
     }))
     .sort((a, b) => (a.position < b.position ? -1 : 1));
 
-  console.log(flow);
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
