1a4e3dd50fc07794745646d77f3da95e405b16ae
Karol Wypchlo <kwypchlo@gmail.com>
2021-02-09 12:39:00 +0100

    enable hydrator

--- packages/dashboard/src/pages/api/hydrator.js
+++ packages/dashboard/src/pages/api/hydrator.js
@@ -3,5 +3,5 @@ export default (req, res) => {
   console.log("body", req.body);
   console.log("cookies", req.cookies);
 
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ name: "John Doe", extra: { foo: "bar" }, header: { foo: ["bar1", "bar2"] } });
 };
