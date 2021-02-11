a35a02087132f38a75d044824043811f842b7ad5
Karol Wypchlo <kwypchlo@gmail.com>
2021-02-11 22:34:37 +0100

    return to test

--- packages/dashboard/src/pages/api/redirect.js
+++ packages/dashboard/src/pages/api/redirect.js
@@ -1,6 +1,22 @@
+import superagent from "superagent";
 import { serialize } from "cookie";
 
-export default (req, res) => {
-  res.setHeader("Set-Cookie", serialize("foo", Math.random(), {}));
+export default async (req, res) => {
+  console.log(Object.keys(req));
+  console.log(req.url);
+  console.log(req.cookies);
+  console.log(req.rawHeaders);
+  try {
+    const auth = await superagent
+      .set("Cookie", serialize("ory_kratos_session", req.cookies.ory_kratos_session))
+      .get("/user");
+
+    console.log(auth.header);
+
+    res.setHeader("Set-Cookie", auth.header["set-cookie"]);
+  } catch (error) {
+    console.log(error);
+  }
+
   res.redirect(302, req.query.return_to);
 };
