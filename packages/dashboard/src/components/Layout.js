3be2ef6690082bbe7776f3eb7367472f552e00fb
Karol Wypchlo <kwypchlo@gmail.com>
2021-02-16 12:35:33 +0100

    logout

--- packages/dashboard/src/components/Layout.js
+++ packages/dashboard/src/components/Layout.js
@@ -142,6 +142,7 @@ export default function Layout({ title, children }) {
                             </a>
                           </Link>
                           <a
+                            href
                             className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                             role="menuitem"
                             onClick={handleSignOut}
@@ -249,6 +250,7 @@ export default function Layout({ title, children }) {
                   </a>
                 </Link>
                 <a
+                  href
                   onClick={handleSignOut}
                   className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
                 >
