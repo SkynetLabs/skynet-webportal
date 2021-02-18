439fb1bea91bdc0ff3bd8dfe2406771db69fae0f
Karol Wypchlo <kwypchlo@gmail.com>
2021-02-18 14:44:16 +0100

    settings

--- packages/dashboard/src/pages/settings.js
+++ packages/dashboard/src/pages/settings.js
@@ -69,6 +69,8 @@ const fieldsConfig = {
 };
 
 export default function Settings({ flow }) {
+  console.log(flow);
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
