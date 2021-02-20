32b64343010ec2d20460b8f512b75e05b1537cd7
Karol Wypchlo <kwypchlo@gmail.com>
2021-02-20 18:39:01 +0100

    clickable logo

--- packages/dashboard/src/config.js
+++ packages/dashboard/src/config.js
@@ -3,7 +3,7 @@ export default {
   kratos: {
     // The URL where ORY Kratos's Public API is located at. If this app and ORY Kratos are running in the same
     // private network, this should be the private network address (e.g. kratos-public.svc.cluster.local)
-    public: process.env.KRATOS_PUBLIC_URL.replace(/\/+$/, ""),
+    public: process.env.NEXT_PUBLIC_KRATOS_PUBLIC_URL.replace(/\/+$/, ""),
     // The URL where ORY Kratos's public API is located, when accessible from the public internet via ORY Oathkeeper.
     // This could be for example http://kratos.my-app.com/.
     browser: process.env.NEXT_PUBLIC_KRATOS_BROWSER_URL.replace(/\/+$/, ""),
