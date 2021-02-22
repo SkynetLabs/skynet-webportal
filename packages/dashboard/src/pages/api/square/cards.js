97b6f0236941e69577b65ec9060c8d3994ef55f6
Karol Wypchlo <kwypchlo@gmail.com>
2021-02-22 18:39:01 +0100

    cards and invoices

--- packages/dashboard/src/pages/api/square/cards.js
+++ packages/dashboard/src/pages/api/square/cards.js
@@ -8,17 +8,45 @@ const client = new Client({
 
 const api = {
   GET: async (req, res) => {
-    console.log(req.headers);
+    const user = "R7R0NY1Z8WT11D43564EEFKTYR"; // req.headers["x-user"];
 
     try {
-      const { result } = await client.customersApi.retrieveCustomer("R7R0NY1Z8WT11D43564EEFKTYR");
+      const { result: customerResult } = await client.customersApi.retrieveCustomer(user);
+      const { customer } = customerResult;
 
-      res.json(result?.customer?.cards ?? []);
+      res.json(customer.cards);
     } catch (error) {
-      // console.log(error);
       res.json([]);
     }
   },
+  // POST: async (req, res) => {
+  //   const user = req.headers["x-user"];
+  //   const card = {
+  //     cardNonce: "YOUR_CARD_NONCE",
+  //     cardholderName: "Amelia Earhart",
+  //     billingAddress: {},
+  //     verificationToken: "verification_token0",
+  //   };
+
+  //   card.bodyBillingAddress.addressLine1 = "500 Electric Ave";
+  //   card.bodyBillingAddress.addressLine2 = "Suite 600";
+  //   card.bodyBillingAddress.addressLine3 = "address_line_38";
+  //   card.bodyBillingAddress.locality = "New York";
+  //   card.bodyBillingAddress.sublocality = "sublocality2";
+  //   card.bodyBillingAddress.administrativeDistrictLevel1 = "NY";
+  //   card.bodyBillingAddress.postalCode = "10003";
+  //   card.bodyBillingAddress.country = "US";
+
+  //   try {
+  //     const { result } = await client.customersApi.createCustomerCard(user, card);
+
+  //     res.status(StatusCodes.NO_CONTENT);
+  //   } catch (error) {
+  //     console.log(Object.keys(error));
+
+  //     res.status(StatusCodes.BAD_REQUEST);
+  //   }
+  // },
 };
 
 export default (req, res) => {
