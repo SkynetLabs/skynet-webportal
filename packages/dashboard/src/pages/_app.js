5a693ca2ffef6582b580e44716adbcda5f9c6491
Karol Wypchlo <kwypchlo@gmail.com>
2021-02-24 15:32:46 +0100

    accounts/**

--- packages/dashboard/src/pages/_app.js
+++ packages/dashboard/src/pages/_app.js
@@ -1,8 +1,16 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import "tailwindcss/tailwind.css";
 import "@fontsource/metropolis/all.css";
 
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Elements stripe={stripePromise}>
+      <Component {...pageProps} />
+    </Elements>
+  );
 }
 
 export default MyApp;
