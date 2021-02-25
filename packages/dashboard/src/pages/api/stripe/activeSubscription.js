74851ae9731bb5c89efb742414683f8979db681b
Karol Wypchlo <kwypchlo@gmail.com>
2021-02-25 15:24:00 +0100

    foo

--- packages/dashboard/src/pages/api/stripe/activeSubscription.js
+++ packages/dashboard/src/pages/api/stripe/activeSubscription.js
@@ -8,7 +8,7 @@ export default async (req, res) => {
   try {
     const authorization = req.headers.authorization; // authorization header from request
     const { stripeCustomerId } = await got("http://accounts:3000/user", { headers: { authorization } }).json();
-    const stripeCustomer = await stripe.customers.retrieve(stripeCustomerId);
+    const stripeCustomer = await stripe.customers.retrieve(stripeCustomerId, { expand: ["subscriptions"] });
     // const { subscriptions } = stripeCustomer;
 
     res.json(stripeCustomer);
