f1caf247889f8440301646f88e18371eb5e02691
Karol Wypchlo <kwypchlo@gmail.com>
2021-03-05 17:31:11 +0100

    payments

--- packages/dashboard/src/pages/auth/registration.js
+++ packages/dashboard/src/pages/auth/registration.js
@@ -87,6 +87,8 @@ export default function Registration({ flow }) {
   }, [fields]);
   const formik = useFormik({ initialValues });
 
+  console.log(formik.values);
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
