c731c488cfa28545b08e40d7af1e4cdc31dc0cb4
Karol Wypchlo <kwypchlo@gmail.com>
2021-03-05 14:03:25 +0100

    payments

--- packages/dashboard/src/pages/payments.js
+++ packages/dashboard/src/pages/payments.js
@@ -21,7 +21,7 @@ const ActiveBadge = () => {
 };
 
 export const getServerSideProps = authServerSideProps(async () => {
-  const prices = await got("api/prices").json();
+  const prices = await got("/api/prices").json();
 
   return { props: { prices } };
 });
