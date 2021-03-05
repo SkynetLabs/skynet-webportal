fcfefe9157de471ab6de61be9a09a62e9d9f7980
Karol Wypchlo <kwypchlo@gmail.com>
2021-03-05 16:45:02 +0100

    payments

--- packages/dashboard/src/pages/index.js
+++ packages/dashboard/src/pages/index.js
@@ -9,47 +9,18 @@ import { SkynetClient } from "skynet-js";
 
 dayjs.extend(relativeTime);
 
-const plans = [
-  {
-    id: "initial_free",
-    tier: 1,
-    stripe: null,
-    name: "Free",
-    price: 0,
-    description: "Unlimited bandwidth with throttled speed",
-  },
-  {
-    id: "initial_plus",
-    tier: 2,
-    stripe: "price_1IO6DLIzjULiPWN6ix1KyCtf",
-    name: "Skynet Plus",
-    price: 5,
-    description: "1 TB premium bandwidth with full speed",
-  },
-  {
-    id: "initial_pro",
-    tier: 3,
-    stripe: "price_1IO6DgIzjULiPWN6NiaSLEKa",
-    name: "Skynet Pro",
-    price: 20,
-    description: "5 TB premium bandwidth with full speed",
-  },
-  {
-    id: "initial_extreme",
-    tier: 4,
-    stripe: "price_1IO6DvIzjULiPWN6wHgK35J4",
-    name: "Skynet Extreme",
-    price: 80,
-    description: "20 TB premium bandwidth with full speed",
-  },
-];
-
+const starter = { id: "free", tier: 1, name: "Free", description: "Unlimited bandwidth with throttled speed" };
 const skynetClient = new SkynetClient(process.env.NEXT_PUBLIC_SKYNET_PORTAL_API);
 const apiPrefix = process.env.NODE_ENV === "development" ? "/api/stubs" : "";
 const fetcher = (url) => fetch(url).then((r) => r.json());
 const isFreeTier = (tier) => tier === 1;
 
-export const getServerSideProps = authServerSideProps();
+export const getServerSideProps = authServerSideProps(async (context, api) => {
+  const stripe = await api.get("stripe/prices").json();
+  const plans = [starter, ...stripe].sort((a, b) => a.tier - b.tier);
+
+  return { props: { plans } };
+});
 
 function SkylinkList({ items = [], timestamp }) {
   return (
@@ -127,7 +98,7 @@ function SkylinkList({ items = [], timestamp }) {
   );
 }
 
-export default function Home() {
+export default function Home({ plans }) {
   const { data: user } = useSWR(`${apiPrefix}/user`, fetcher);
   const { data: stats } = useSWR(`${apiPrefix}/user/stats`, fetcher);
   const { data: downloads } = useSWR(`${apiPrefix}/user/downloads?pageSize=3&offset=0`, fetcher);
