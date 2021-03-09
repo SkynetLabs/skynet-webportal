ed6e7fe7a37650f948d32e44d8d9fc90d15f369d
Karol Wypchlo <kwypchlo@gmail.com>
2021-03-09 17:50:31 +0100

    payments

--- packages/dashboard/src/components/Form/SelfServiceForm.js
+++ packages/dashboard/src/components/Form/SelfServiceForm.js
@@ -1,4 +1,5 @@
 import { useFormik, getIn, setIn } from "formik";
+import classnames from "classnames";
 import Message from "./Message";
 
 export default function SelfServiceForm({ config, fieldsConfig, title, button = "Submit" }) {
@@ -15,12 +16,10 @@ export default function SelfServiceForm({ config, fieldsConfig, title, button = 
       <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
         <form className="space-y-6" action={config.action} method={config.method}>
           {fields.map((field) => (
-            <div key={field.name}>
-              {field.type !== "hidden" && (
-                <label htmlFor={field.name} className="block text-sm font-medium text-gray-700 mb-1">
-                  {fieldsConfig[field.name]?.label ?? field.name}
-                </label>
-              )}
+            <div key={field.name} className={classnames({ hidden: field.type === "hidden" })}>
+              <label htmlFor={field.name} className="block text-sm font-medium text-gray-700 mb-1">
+                {fieldsConfig[field.name]?.label ?? field.name}
+              </label>
               <div>
                 <input
                   id={field.type === "hidden" ? null : field.name}
@@ -31,8 +30,22 @@ export default function SelfServiceForm({ config, fieldsConfig, title, button = 
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
                   value={getIn(formik.values, field.name) ?? ""}
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+                  className={classnames(
+                    "appearance-none block w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none sm:text-sm",
+                    {
+                      "border-gray-300 placeholder-gray-400 focus:ring-green-500 focus:border-green-500": !Boolean(
+                        field?.messages?.length
+                      ),
+                      "border-red-300 text-red-900 placeholder-red-300 focus:ring-red-500 focus:border-red-500": Boolean(
+                        field?.messages?.length
+                      ),
+                    }
+                  )}
                 />
+
+                {field?.messages?.map(({ text }) => (
+                  <p className="mt-2 text-sm text-red-600">{text}</p>
+                ))}
 
                 {field.checks && (
                   <div className="mt-4">
@@ -72,12 +85,6 @@ export default function SelfServiceForm({ config, fieldsConfig, title, button = 
                     </ul>
                   </div>
                 )}
-
-                {Boolean(field.messages?.length) && (
-                  <div className="mt-2">
-                    <Message items={field.messages.map(({ text }) => text)} />
-                  </div>
-                )}
               </div>
             </div>
           ))}
@@ -89,7 +96,9 @@ export default function SelfServiceForm({ config, fieldsConfig, title, button = 
             {button}
           </button>
 
-          {Boolean(config.errors?.length) && <Message items={config.errors.map(({ message }) => message)} />}
+          {config?.errors?.map(({ message }) => (
+            <p className="mt-2 text-sm text-red-600">{message}</p>
+          ))}
         </form>
       </div>
     </div>
