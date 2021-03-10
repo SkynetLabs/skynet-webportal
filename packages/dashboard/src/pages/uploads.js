98a16d80c33b45c877a187a5de7bf27007487026
Karol Wypchlo <kwypchlo@gmail.com>
2021-03-10 15:50:10 +0100

    payments

--- packages/dashboard/src/pages/uploads.js
+++ packages/dashboard/src/pages/uploads.js
@@ -29,7 +29,6 @@ export default function Uploads({ initialData }) {
   const [offset, setOffset] = useState(0);
   const { data } = useAccountsApi(`${apiPrefix}/user/uploads?pageSize=10&offset=${offset}`, {
     initialData,
-    revalidateOnMount: true,
   });
 
   return (
