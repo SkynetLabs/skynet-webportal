33350063bb71719451acf940e9ca7a5ea3ca56e1
Karol Wypchlo <kwypchlo@gmail.com>
2021-03-24 13:41:40 +0100

    fix settings link

--- packages/dashboard/src/components/Layout.js
+++ packages/dashboard/src/components/Layout.js
@@ -281,7 +281,7 @@ export default function Layout({ title, children }) {
                 </div>
               </div> */}
               <div className="mt-3 px-2 space-y-1">
-                <Link href="/">
+                <Link href="/settings">
                   <a className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">
                     Settings
                   </a>
