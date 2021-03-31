31ba4b29ed82dcccc385cf9897f9bfb16bb8b51c
Ivaylo Novakov <inovakov@gmail.com>
2021-03-31 15:15:24 +0200

    Merge the db backup scripts into on and make them configurable.

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -13,6 +13,22 @@ if [[ $AWS_ACCESS_KEY_ID == "" || $AWS_SECRET_ACCESS_KEY == "" ]]; then
 fi
 # Take the current datetime:
 DT=`date +%Y-%m-%d`
+
+### COCKROACH DB ###
+# Check if a backup already exists:
+totalFoundObjects=$(aws s3 ls s3://skynet-crdb-backups/backups/cockroach/ --recursive --summarize | grep "$DT" | wc -l)
+if [ "$totalFoundObjects" -eq "1" ]; then
+   echo "Backup already exists for today. Exiting."
+   exit 0
+fi
+# Create a cockroachdb backup:
+docker exec cockroach \
+  cockroach sql \
+  --host cockroach:26257 \
+  --certs-dir=/certs \
+  --execute="BACKUP TO 's3://skynet-crdb-backups/backups/cockroach/$DT?AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID&AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY';"
+
+### MONGO DB ###
 # Check if a backup already exists:
 totalFoundObjects=$(aws s3 ls s3://skynet-crdb-backups/backups/mongo/ --recursive --summarize | grep "$DT.tgz" | wc -l)
 if [ "$totalFoundObjects" -eq "1" ]; then
