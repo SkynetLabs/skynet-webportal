6001484e7107189fcbb12eccd7ad2251021a6001
Ivaylo Novakov <inovakov@gmail.com>
2021-03-31 15:31:56 +0200

    Use a more resilient approach to reading the .env file.

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -2,9 +2,12 @@
 
 # Get current working directory (pwd doesn't cut it)
 cwd=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-# Set the environment:
+# Set the environment. We only grab the entries we need because otherwise we
+# need to deal with the edge cases presented by problematic values.
 set -o allexport
-source $cwd/../.env
+cat $cwd/../.env | grep "AWS_ACCESS_KEY_ID\|AWS_SECRET_ACCESS_KEY\|S3_BACKUP_PATH" > .tmpenv
+source .tmpenv
+rm .tmpenv
 set +o allexport
 # Check for AWS credentials:
 if [[ $AWS_ACCESS_KEY_ID == "" || $AWS_SECRET_ACCESS_KEY == "" ]]; then
@@ -37,25 +40,25 @@ if [[ $? > 0 ]]; then
   exit 1
 fi
 
-#### MONGO DB ###
-## Check if a backup already exists:
-#totalFoundObjects=$(aws s3 ls s3://skynet-crdb-backups/backups/mongo/ --recursive --summarize | grep "$DT.tgz" | wc -l)
-#if [ "$totalFoundObjects" -eq "1" ]; then
-#   echo "Backup already exists for today. Exiting."
-#   exit 0
-#fi
-## Create the backup:
-#docker exec mongo \
-#  mongodump \
-#  -o /data/db/backups/$DT \
-#  mongodb://$SKYNET_DB_USER:$SKYNET_DB_PASS@$SKYNET_DB_HOST:$SKYNET_DB_PORT
-#if [[ $? > 0 ]]; then
-#  echo "Creating a MongoDB backup failed. Exiting."
-#  exit 1
-#fi
-## Compress the backup:
-#cd $cwd/../docker/data/mongo/db/backups/ && tar -czf $DT.tgz $DT && cd -
-## Upload the backup to S3:
-#aws s3 cp $DT.tgz s3://skynet-crdb-backups/backups/mongo/
-## Clean up
-#rm -rf $DT.tgz $cwd/../docker/data/mongo/db/backups/$DT
+### MONGO DB ###
+# Check if a backup already exists:
+totalFoundObjects=$(aws s3 ls s3://skynet-crdb-backups/backups/mongo/ --recursive --summarize | grep "$DT.tgz" | wc -l)
+if [ "$totalFoundObjects" -eq "1" ]; then
+   echo "Backup already exists for today. Exiting."
+   exit 0
+fi
+# Create the backup:
+docker exec mongo \
+  mongodump \
+  -o /data/db/backups/$DT \
+  mongodb://$SKYNET_DB_USER:$SKYNET_DB_PASS@$SKYNET_DB_HOST:$SKYNET_DB_PORT
+if [[ $? > 0 ]]; then
+  echo "Creating a MongoDB backup failed. Exiting."
+  exit 1
+fi
+# Compress the backup:
+cd $cwd/../docker/data/mongo/db/backups/ && tar -czf $DT.tgz $DT && cd -
+# Upload the backup to S3:
+aws s3 cp $DT.tgz s3://skynet-crdb-backups/backups/mongo/
+# Clean up
+rm -rf $DT.tgz $cwd/../docker/data/mongo/db/backups/$DT
