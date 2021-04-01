e0844aa18b193207e05ba4966f3a74e9c52d7056
Ivaylo Novakov <inovakov@gmail.com>
2021-04-01 13:45:58 +0200

    Mongo restore fixed.

--- scripts/db_restore.sh
+++ scripts/db_restore.sh
@@ -78,25 +78,27 @@ else
   fi
 fi
 
-#### MONGO DB ###
-## Check if the backup exists:
-#totalFoundObjects=$(aws s3 ls $S3_BACKUP_PATH/$BACKUP --recursive --summarize | grep "mongo.tgz" | wc -l)
-#if [ "$totalFoundObjects" -eq "0" ]; then
-#  echo "This backup doesn't exist!"
-#  exit 1
-#fi
-## Get the backup from S3:
-#aws s3 cp $S3_BACKUP_PATH/$BACKUP/mongo.tgz mongo.tgz
-## Prepare a clean `to_restore` dir:
-#rm -rf $cwd/../docker/data/mongo/db/backups/to_restore
-#mkdir -p $cwd/../docker/data/mongo/db/backups/to_restore
-## Decompress the backup:
-#tar -xzf mongo.tgz -C $cwd/../docker/data/mongo/db/backups/to_restore
-#rm mongo.tgz
-## Restore the backup:
-#docker exec mongo \
-#  mongorestore \
-#  mongodb://$SKYNET_DB_USER:$SKYNET_DB_PASS@$SKYNET_DB_HOST:$SKYNET_DB_PORT \
-#  /data/db/backups/to_restore/mongo
-## Clean up:
-#rm -rf $cwd/../docker/data/mongo/db/backups/to_restore
+### MONGO DB ###
+# Check if the backup exists:
+totalFoundObjects=$(aws s3 ls $S3_BACKUP_PATH/$BACKUP --recursive --summarize | grep "mongo.tgz" | wc -l)
+if [ "$totalFoundObjects" -eq "0" ]; then
+  echo "This backup doesn't exist!"
+  exit 1
+fi
+# Get the backup from S3:
+aws s3 cp $S3_BACKUP_PATH/$BACKUP/mongo.tgz mongo.tgz
+# Prepare a clean `to_restore` dir:
+rm -rf $cwd/../docker/data/mongo/db/backups/to_restore
+mkdir -p $cwd/../docker/data/mongo/db/backups/to_restore
+# Decompress the backup:
+tar -xzf mongo.tgz -C $cwd/../docker/data/mongo/db/backups/to_restore
+rm mongo.tgz
+# Restore the backup:
+# The name of the backup is not `mongo` due to the way we're creating it,
+# it's $BACKUP.
+docker exec mongo \
+  mongorestore \
+  mongodb://$SKYNET_DB_USER:$SKYNET_DB_PASS@$SKYNET_DB_HOST:$SKYNET_DB_PORT \
+  /data/db/backups/to_restore/$BACKUP
+# Clean up:
+rm -rf $cwd/../docker/data/mongo/db/backups/to_restore
