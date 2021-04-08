b4231abfb6372ad183e06f8ea487233e0cef3726
Karol Wypchlo <kwypchlo@gmail.com>
2021-04-08 13:18:29 +0200

    use authenticated status

--- packages/website/src/services/useAuthenticatedStatus.js
+++ packages/website/src/services/useAuthenticatedStatus.js
@@ -1,5 +1,5 @@
 import useSWR from "swr";
 
 export default function useAuthenticatedStatus() {
-  return useSWR("/__internal/do/not/use", (url) => (await fetch(url)).json());
+  return useSWR("/__internal/do/not/use", (url) => fetch(url).then((response) => response.json()));
 }
