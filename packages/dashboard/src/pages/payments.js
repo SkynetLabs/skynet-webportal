26b3ec2cbc1c76c00363d4be4be9174c4f928949
Karol Wypchlo <kwypchlo@gmail.com>
2021-04-08 12:35:47 +0200

    change storageUsed => totalUploadsSize

--- packages/dashboard/src/pages/payments.js
+++ packages/dashboard/src/pages/payments.js
@@ -83,7 +83,9 @@ export default function Payments({ plans, user: initialUserData, stats: initialS
             <div className="bg-white overflow-hidden shadow rounded-lg">
               <div className="px-4 py-5 sm:p-6">
                 <dt className="text-sm font-medium text-gray-500 truncate">Storage used</dt>
-                <dd className="mt-1 text-3xl font-semibold text-gray-900">{prettyBytes(stats.storageUsed)}</dd>
+                <dd className="mt-1 text-3xl font-semibold text-gray-900">
+                  {prettyBytes(stats?.totalUploadsSize ?? 0)}
+                </dd>
               </div>
             </div>
           </dl>
