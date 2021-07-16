141fdd67ec4ffeb58178550e8c2d6bc73044a651
Karol Wypchlo <kwypchlo@gmail.com>
2021-07-16 13:31:39 +0200

    add required libraries

--- setup-scripts/setup-health-check-scripts.sh
+++ setup-scripts/setup-health-check-scripts.sh
@@ -5,7 +5,7 @@ set -e # exit on first error
 sudo apt-get update
 sudo apt-get -y install python3-pip
 
-pip3 install DiscordWebhook python-dotenv requests elasticsearch-curator
+pip3 install --upgrade DiscordWebhook python-dotenv requests elasticsearch-curator google-api-python-client google-auth-httplib2 google-auth-oauthlib
 
 # add cron entries to user crontab
 crontab -u user /home/user/skynet-webportal/setup-scripts/support/crontab
