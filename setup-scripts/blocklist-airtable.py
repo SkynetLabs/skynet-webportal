71f9d5280e66a11ffa25365440c127077a646b27
Karol Wypchło <kwypchlo@gmail.com>
2021-07-16 13:12:58 +0200

    use webhook instead of discrod bot to send messages (#979)
    * initial refactor
    
    * do not use before define
    
    * forgot to remove client
    
    * test notification
    
    * add /cc
    
    * fix /cc
    
    * fix /cc role
    
    * fix /cc
    
    * test file upload
    
    * test file upload
    
    * test file upload
    
    * default to no mentions
    
    * unformat
    
    * replace discord with DiscordWebhook
    
    * add readme
    
    * don't fail on failures in message send

--- setup-scripts/blocklist-airtable.py
+++ setup-scripts/blocklist-airtable.py
@@ -3,13 +3,19 @@
 import traceback, os, re, asyncio, requests, json, discord
 from bot_utils import setup, send_msg
 
-bot_token = setup()
-client = discord.Client()
+setup()
 
 AIRTABLE_API_KEY = os.getenv("AIRTABLE_API_KEY")
 AIRTABLE_BASE = os.getenv("AIRTABLE_BASE", "app89plJvA9EqTJEc")
 AIRTABLE_TABLE = os.getenv("AIRTABLE_TABLE", "Table%201")
 AIRTABLE_FIELD = os.getenv("AIRTABLE_FIELD", "Link")
+
+async def run_checks():
+    try:
+        await block_skylinks_from_airtable()
+    except:  # catch all exceptions
+        trace = traceback.format_exc()
+        await send_msg("```\n{}\n```".format(trace), force_notify=True)
 
 
 def exec(command):
@@ -33,7 +39,7 @@ async def block_skylinks_from_airtable():
             status_code = str(response.status_code)
             response_text = response.text or "empty response"
             message = "Airtable blocklist integration responded with code " + status_code + ": " + response_text
-            return print(message) or await send_msg(client, message, force_notify=False)
+            return await send_msg(message, force_notify=False)
 
         data = response.json()
 
@@ -53,7 +59,7 @@ async def block_skylinks_from_airtable():
     if len(skylinks_returned) != len(skylinks):
         invalid_skylinks = [str(skylink) for skylink in list(set(skylinks_returned) - set(skylinks))]
         message = str(len(invalid_skylinks)) + " of the skylinks returned from Airtable are not valid"
-        print(message) or await send_msg(client, message, file=("\n".join(invalid_skylinks)))
+        await send_msg(message, file=("\n".join(invalid_skylinks)))
 
     apipassword = exec("docker exec sia cat /sia-data/apipassword")
     ipaddress = exec("docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sia")
@@ -72,7 +78,7 @@ async def block_skylinks_from_airtable():
         status_code = str(response.status_code)
         response_text = response.text or "empty response"
         message = "Siad blocklist endpoint responded with code " + status_code + ": " + response_text
-        return print(message) or await send_msg(client, message, force_notify=False)
+        return await send_msg(message, force_notify=False)
 
     print("Searching nginx cache for blocked files")
     cached_files_count = 0
@@ -89,25 +95,11 @@ async def block_skylinks_from_airtable():
 
     exec('docker exec -it nginx bash -c "' + cached_files_command + ' | xargs rm"')
     message = "Purged " + str(cached_files_count) + " blocklisted files from nginx cache"
-    return print(message) or await send_msg(client, message)
+    return await send_msg(message)
 
 
-async def exit_after(delay):
-    await asyncio.sleep(delay)
-    os._exit(0)
-
-
-@client.event
-async def on_ready():
-    try:
-        await block_skylinks_from_airtable()
-    except:  # catch all exceptions
-        message = "```\n{}\n```".format(traceback.format_exc())
-        await send_msg(client, message, force_notify=False)
-    asyncio.create_task(exit_after(3))
-
-
-client.run(bot_token)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run_checks())
 
 # --- BASH EQUIVALENT
 # skylinks=$(curl "https://api.airtable.com/v0/${AIRTABLE_BASE}/${AIRTABLE_TABLE}?fields%5B%5D=${AIRTABLE_FIELD}" -H "Authorization: Bearer ${AIRTABLE_KEY}" | python3 -c "import sys, json; print('[\"' + '\",\"'.join([entry['fields']['Link'] for entry in json.load(sys.stdin)['records']]) + '\"]')")
