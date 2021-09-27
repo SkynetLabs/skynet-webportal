f1cba47e2c258b95b58d04704c8c8af6489cd830
Karol Wypchlo <kwypchlo@gmail.com>
2021-09-27 20:21:44 +0200

    default

--- packages/ipfs-api/src/consts.ts
+++ packages/ipfs-api/src/consts.ts
@@ -1,10 +1,8 @@
-export const API_HOSTNAME = process.env.API_HOSTNAME || "0.0.0.0";
-export const API_PORT = process.env.API_PORT || "3100";
-export const UPLOAD_PATH = process.env.UPLOAD_PATH || "/tmp";
-export const IPFS_INFURA_API =
-  process.env.IPFS_INFURA_API || "https://ipfs.infura.io:5001";
-export const IPFS_GATEWAY =
-  process.env.IPFS_GATEWAY || "https://cloudflare-ipfs.com/ipfs/";
-export const SKYNET_PORTAL = process.env.SKYNET_PORTAL || "https://siasky.net";
+export const API_HOSTNAME = "0.0.0.0";
+export const API_PORT = "3100";
+export const UPLOAD_PATH = "/tmp";
+export const IPFS_INFURA_API = "https://ipfs.infura.io:5001";
+export const IPFS_GATEWAY = "https://cloudflare-ipfs.com/ipfs/";
+export const SKYNET_PORTAL = "https://siasky.net";
 export const MONGO_CONNECTIONSTRING = `mongodb://${process.env.IPFS_MONGO_USERNAME}:${process.env.IPFS_MONGO_PASSWORD}@ipfs-mongo:27017`;
 export const MONGO_DBNAME = "ipfs-to-skynet";
