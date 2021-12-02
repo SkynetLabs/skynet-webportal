b573edad17e6876b6a7c818092f9499548186de5
Matthew Sevey <mjsevey@gmail.com>
2021-12-02 15:57:53 -0500

    testing json

--- setup-scripts/cpu-disk-usage.sh
+++ setup-scripts/cpu-disk-usage.sh
@@ -9,20 +9,26 @@ top of the file and the file can easily be truncated if needed.
 
 # Define Loop Interval
 loop_interval=60
+logfile="cpu-disk-usage.log"
+jsonfile="cpu-disk-usage.json"
 
 # Write the output in an infinite loop.
 while true; do
     # CPU usage
     cpu=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}') 
-    sed -i "1iCPU: ${cpu}" usage.txt
+    sed -i "1iCPU: ${cpu}" $logfile
 
     # Disk Usage
     disk=$(df -Ph . | tail -1 | awk '{print $4}')
-    sed -i "1iDISK: ${disk}" usage.txt
+    sed -i "1iDISK: ${disk}" $logfile
     
     # Write the timestamp
     timestamp=$(date)
-    sed -i "1iTIMESTAMP: ${timestamp}" usage.txt
+    sed -i "1iTIMESTAMP: ${timestamp}" $logfile
+
+    # Write and copy file to nginx docker container to serve
+    printf '{"cpu":"%s","disk":"%s","timestamp":"%s"}' "$cpu" "$disk" "$timestamp" > $jsonfile
+    docker cp $jsonfile nginx:/data/stats.json
 
     # Sleep
     sleep $loop_interval
