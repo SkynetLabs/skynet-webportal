c3c243229ac51976db4d49cf23fb6198fa6ff0c7
Karol Wypchlo <kwypchlo@gmail.com>
2021-12-14 15:14:36 +0100

    maintenance page for accounts

--- packages/dashboard/tailwind.config.js
+++ packages/dashboard/tailwind.config.js
@@ -40,6 +40,15 @@ module.exports = {
       },
       backgroundColor: ["disabled"],
       textColor: ["disabled"],
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 3s ease-in-out infinite",
+      },
     },
   },
   variants: {
