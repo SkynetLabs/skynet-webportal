5065e66b466d00a477138a194a85b75e551bce37
Karol Wypchlo <kwypchlo@gmail.com>
2022-01-13 21:41:56 +0100

    support auth only portal mode in health-check module

--- packages/health-check/src/utils.js
+++ packages/health-check/src/utils.js
@@ -1,3 +1,5 @@
+const got = require("got");
+
 /**
  * Get the time between start and now in milliseconds
  */
@@ -39,9 +41,60 @@ function ensureValidJSON(object) {
   return JSON.parse(stringified);
 }
 
+/**
+ * Authenticate with given credentials and return auth cookie
+ * Creates new account if username does not exist
+ * Only authenticates when portal is set to authenticated users only mode
+ */
+function getAuthCookie() {
+  // cache auth promise so only one actual request will be made
+  if (getAuthCookie.cache) return getAuthCookie.cache;
+
+  // do not authenticate if it is not necessary
+  if (process.env.ACCOUNTS_LIMIT_ACCESS !== "authenticated") return {};
+
+  const email = process.env.ACCOUNTS_TEST_USER_EMAIL;
+  const password = process.env.ACCOUNTS_TEST_USER_PASSWORD;
+
+  if (!email) throw new Error("ACCOUNTS_TEST_USER_EMAIL cannot be empty");
+  if (!password) throw new Error("ACCOUNTS_TEST_USER_PASSWORD cannot be empty");
+
+  async function authenticate() {
+    try {
+      // authenticate with given test credentials
+      const response = await got.post(`${process.env.SKYNET_DASHBOARD_URL}/api/login`, {
+        json: { email, password },
+      });
+
+      // extract set-cookie from successful authentication request
+      const cookies = response.headers["set-cookie"];
+
+      // find the skynet-jwt cookie
+      const jwtcookie = cookies.find((cookie) => cookie.startsWith("skynet-jwt"));
+
+      // extract just the cookie value (no set-cookie props) from set-cookie
+      return jwtcookie.match(/skynet-jwt=[^;]+;/)[0];
+    } catch (error) {
+      // 401 means that service worked but user could not have been authenticated
+      if (error.response && error.response.statusCode === 401) {
+        // sign up with the given credentials
+        await got.post(`${process.env.SKYNET_DASHBOARD_URL}/api/user`, {
+          json: { email, password },
+        });
+
+        // retry authentication
+        return authenticate();
+      }
+    }
+  }
+
+  return (getAuthCookie.cache = authenticate());
+}
+
 module.exports = {
   calculateElapsedTime,
   getYesterdayISOString,
   getResponseContent,
   ensureValidJSON,
+  getAuthCookie,
 };
