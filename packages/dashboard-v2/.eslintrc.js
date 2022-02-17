48714220607f88f270709a253bf2be8765df6db6
Michał Leszczyk <michasko37@gmail.com>
2022-02-17 12:50:59 +0100

    ops(dashboard-v2): setup Storybook

--- packages/dashboard-v2/.eslintrc.js
+++ packages/dashboard-v2/.eslintrc.js
@@ -2,5 +2,5 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: true,
   },
-  extends: "react-app",
-};
+  extends: ['react-app', 'plugin:storybook/recommended'],
+}
---
+++ packages/dashboard-v2/.storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+  core: {
+    builder: 'webpack5',
+  },
+}
