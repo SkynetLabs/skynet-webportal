0c275a8f3ed6aab9da216a35e92834a90abe0f61
Michał Leszczyk <michasko37@gmail.com>
2022-02-23 10:41:20 +0100

    fix(dashboard-v2): fix active tab indicator after window resize

--- packages/dashboard-v2/src/components/Tabs/ActiveTabIndicator.js
+++ packages/dashboard-v2/src/components/Tabs/ActiveTabIndicator.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import { useWindowSize } from "react-use";
 
 const Wrapper = styled.div.attrs({
   className: "absolute left-0 bottom-0 w-full h-0.5 bg-palette-200",
@@ -15,6 +16,7 @@ const Indicator = styled.div.attrs({
 export const ActiveTabIndicator = ({ tabRef }) => {
   const [position, setPosition] = useState(0);
   const [width, setWidth] = useState(0);
+  const { width: windowWidth } = useWindowSize();
 
   useEffect(() => {
     if (!tabRef?.current) {
@@ -24,7 +26,7 @@ export const ActiveTabIndicator = ({ tabRef }) => {
     const { offsetLeft, offsetWidth } = tabRef.current;
     setPosition(offsetLeft);
     setWidth(offsetWidth);
-  }, [tabRef]);
+  }, [tabRef, windowWidth]);
 
   return (
     <Wrapper>
