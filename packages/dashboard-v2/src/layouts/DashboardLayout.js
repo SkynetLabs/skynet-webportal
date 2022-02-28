606351005652b4f2cee6bea036160ac2ff92fa75
Michał Leszczyk <michasko37@gmail.com>
2022-02-28 16:39:50 +0100

    feat(dashboard-v2): API access

--- packages/dashboard-v2/src/layouts/DashboardLayout.js
+++ packages/dashboard-v2/src/layouts/DashboardLayout.js
@@ -1,5 +1,8 @@
 import * as React from "react";
 import styled from "styled-components";
+import { SWRConfig } from "swr";
+
+import { authenticatedOnly } from "../lib/swrConfig";
 
 import { PageContainer } from "../components/PageContainer";
 import { NavBar } from "../components/Navbar";
@@ -16,13 +19,15 @@ const Layout = styled.div.attrs({
 const DashboardLayout = ({ children }) => {
   return (
     <>
-      <Layout>
-        <NavBar />
-        <PageContainer>
-          <main className="mt-14">{children}</main>
-        </PageContainer>
-        <Footer />
-      </Layout>
+      <SWRConfig value={authenticatedOnly}>
+        <Layout>
+          <NavBar />
+          <PageContainer>
+            <main className="mt-14">{children}</main>
+          </PageContainer>
+          <Footer />
+        </Layout>
+      </SWRConfig>
     </>
   );
 };
