476ee040d8ca89b1f63e9e8986a2a357731d1b12
Karol Wypchło <kwypchlo@gmail.com>
2022-03-10 10:14:34 +0100

    Merge pull request #1854 from SkynetLabs/revert-permanent-301-redirect
    Revert permanent 301 redirect

--- docker/nginx/libs/skynet/skylink.lua
+++ docker/nginx/libs/skynet/skylink.lua
@@ -3,26 +3,12 @@ local _M = {}
 local basexx = require("basexx")
 local hasher = require("hasher")
 
--- use lowercase alphabet since our skylinks are part of urls
-local base32_alphabet = "0123456789abcdefghijklmnopqrstuv"
-
 -- parse any skylink and return base64 version
-function _M.base64(skylink)
+function _M.parse(skylink)
     if string.len(skylink) == 55 then
-        local decoded = basexx.from_basexx(string.lower(skylink), base32_alphabet, 5)
+        local decoded = basexx.from_basexx(string.upper(skylink), "0123456789ABCDEFGHIJKLMNOPQRSTUV", 5)
 
         return basexx.to_url64(decoded)
-    end
-
-    return skylink
-end
-
--- parse any skylink and return base32 version
-function _M.base32(skylink)
-    if string.len(skylink) == 46 then
-        local decoded = basexx.from_url64(skylink)
-
-        return basexx.to_basexx(decoded, base32_alphabet, 5)
     end
 
     return skylink
@@ -31,7 +17,7 @@ end
 -- hash skylink into 32 bytes hash used in blocklist
 function _M.hash(skylink)
     -- ensure that the skylink is base64 encoded
-    local base64Skylink = _M.base64(skylink)
+    local base64Skylink = _M.parse(skylink)
 
     -- decode skylink from base64 encoding
     local rawSkylink = basexx.from_url64(base64Skylink)
