86a7f0358b5c82dca4c2cb53d42d079caffa7022
Michał Leszczyk <michasko37@gmail.com>
2022-03-15 15:58:02 +0100

    feat(dashboard-v2): add Settings/Notifications page

---
+++ packages/dashboard-v2/src/pages/settings/index.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+import UserSettingsLayout from "../../layouts/UserSettingsLayout";
+
+const SettingsPage = () => {
+  return (
+    <>
+      <h4>Account</h4>
+    </>
+  );
+};
+
+SettingsPage.Layout = UserSettingsLayout;
+
+export default SettingsPage;
