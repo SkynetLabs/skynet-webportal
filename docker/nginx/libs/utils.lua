8623f53d9dd50a11e239159950856b9287c1a2da
Karol Wypchlo <kwypchlo@gmail.com>
2022-03-15 22:56:24 +0100

    export is_table_empty from utils module

--- docker/nginx/libs/utils.lua
+++ docker/nginx/libs/utils.lua
@@ -1,7 +1,7 @@
 local _M = {}
 
 -- utility function for checking if table is empty
-function is_table_empty(check)
+function _M.is_table_empty(check)
     -- bind next to local variable to achieve ultimate efficiency
     -- https://stackoverflow.com/a/1252776
     local next = next
