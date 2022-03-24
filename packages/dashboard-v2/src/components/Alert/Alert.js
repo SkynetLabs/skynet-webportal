60c2d35eb9b81ce2e314a8879d84a718a245bea6
Michał Leszczyk <michasko37@gmail.com>
2022-03-24 10:32:29 +0100

    feat(dashboard-v2): add Alert component

---
+++ packages/dashboard-v2/src/components/Alert/Alert.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import cn from "classnames";
+
+export const Alert = styled.div.attrs(({ $variant }) => ({
+  className: cn("px-3 py-2 sm:px-6 sm:py-4 rounded border", {
+    "bg-red-100 border-red-200 text-error": $variant === "error",
+    "bg-green-100 border-green-200 text-palette-400": $variant === "success",
+  }),
+}))``;
