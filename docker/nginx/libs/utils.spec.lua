1cce3f8076694c8cb9abaa7fa03c11e588cff9cc
Karol Wypchlo <kwypchlo@gmail.com>
2022-03-25 14:21:21 +0100

    add luacheck job

--- docker/nginx/libs/utils.spec.lua
+++ docker/nginx/libs/utils.spec.lua
@@ -15,31 +15,31 @@ describe("extract_cookie", function()
 
     it("should return nil if cookie string is nil", function()
         local cookie = utils.extract_cookie_value(nil, "aaa")
-        
+
         assert.is_nil(cookie)
     end)
 
     it("should return nil if cookie name is not found", function()
         local cookie = utils.extract_cookie(cookie_string, "foo")
-        
+
         assert.is_nil(cookie)
     end)
 
     it("should return cookie if cookie_string starts with that cookie name", function()
         local cookie = utils.extract_cookie(cookie_string, "aaa")
-        
+
         assert.are.equals(cookie, "aaa=bbb")
     end)
 
     it("should return cookie if cookie_string ends with that cookie name", function()
         local cookie = utils.extract_cookie(cookie_string, "xxx")
-        
+
         assert.are.equals(cookie, "xxx=yyy")
     end)
 
     it("should return cookie with custom matcher", function()
         local cookie = utils.extract_cookie(cookie_string, "skynet[-]jwt")
-        
+
         assert.are.equals(cookie, "skynet-jwt=MTY0NzUyr8jD-ytiWtspm0tGabKfooxeIDuWcXhJ3lnY0eEw==")
     end)
 end)
@@ -49,31 +49,31 @@ describe("extract_cookie_value", function()
 
     it("should return nil if cookie string is nil", function()
         local value = utils.extract_cookie_value(nil, "aaa")
-        
+
         assert.is_nil(value)
     end)
 
     it("should return nil if cookie name is not found", function()
         local value = utils.extract_cookie_value(cookie_string, "foo")
-        
+
         assert.is_nil(value)
     end)
 
     it("should return value if cookie_string starts with that cookie name", function()
         local value = utils.extract_cookie_value(cookie_string, "aaa")
-        
+
         assert.are.equals(value, "bbb")
     end)
 
     it("should return cookie if cookie_string ends with that cookie name", function()
         local value = utils.extract_cookie_value(cookie_string, "xxx")
-        
+
         assert.are.equals(value, "yyy")
     end)
 
     it("should return cookie with custom matcher", function()
         local value = utils.extract_cookie_value(cookie_string, "skynet[-]jwt")
-        
+
         assert.are.equals(value, "MTY0NzUyr8jD-ytiWtspm0tGabKfooxeIDuWcXhJ3lnY0eEw==")
     end)
 end)
