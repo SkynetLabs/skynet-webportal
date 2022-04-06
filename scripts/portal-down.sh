5473b8475a9052fb23571ccbb979ac030d9b6761
Ivaylo Novakov <inovakov@gmail.com>
2022-04-06 11:59:28 +0200

    Update portal control scripts, so they can talk to the new health-check container.

--- scripts/portal-down.sh
+++ scripts/portal-down.sh
@@ -2,15 +2,17 @@
 
 set -e # exit on first error
 
-while getopts d:t: flag
+while getopts d:t:r: flag
 do
     case "${flag}" in
         d) delay=${OPTARG};;
         t) timeout=${OPTARG};;
+        r) reason=${OPTARG};;
     esac
 done
 delay=${delay:-0} # default to no delay
 timeout=${timeout:-300} # default timeout is 300s
+reason=${reason:-"disabled manually"}
 
 countdown() {
     local secs=$1
@@ -25,7 +27,7 @@ countdown() {
 countdown $delay
 
 # stop healh-check so the server is taken our of load balancer
-docker exec health-check cli/disable
+docker exec health-check cli disable $reason
 
 # then wait 5 minutes for the load balancer to propagate the dns records
 countdown $timeout
