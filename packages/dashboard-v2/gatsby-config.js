d15d2146f5f17c8a5b96d906ef7cdb36b9648a4c
Michał Leszczyk <michasko37@gmail.com>
2022-04-12 12:55:09 +0200

    ops(dashboard-v2): improve local developer experience

--- packages/dashboard-v2/gatsby-config.js
+++ packages/dashboard-v2/gatsby-config.js
@@ -1,9 +1,15 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const { GATSBY_PORTAL_DOMAIN } = process.env;
 
 module.exports = {
   siteMetadata: {
-    title: "Skynet Account",
-    siteUrl: `https://account.${process.env.GATSBY_PORTAL_DOMAIN}/`,
+    title: `Accounts Dashboard`,
+    siteUrl: `https://account.${GATSBY_PORTAL_DOMAIN}`,
   },
   trailingSlash: "never",
   plugins: [
@@ -24,12 +30,26 @@ module.exports = {
     },
   ],
   developMiddleware: (app) => {
+    // Proxy Accounts service API requests:
     app.use(
       "/api/",
       createProxyMiddleware({
-        target: "https://account.skynetpro.net",
+        target: `https://account.${GATSBY_PORTAL_DOMAIN}`,
         secure: false, // Do not reject self-signed certificates.
         changeOrigin: true,
+      })
+    );
+
+    // Proxy /skynet requests (e.g. uploads)
+    app.use(
+      ["/skynet", "/__internal/"],
+      createProxyMiddleware({
+        target: `https://${GATSBY_PORTAL_DOMAIN}`,
+        secure: false, // Do not reject self-signed certificates.
+        changeOrigin: true,
+        pathRewrite: {
+          "^/skynet": "",
+        },
       })
     );
   },
