7248147ba72f127f6f37094dbad1404d53669c65
Michał Leszczyk <michasko37@gmail.com>
2022-04-15 16:19:04 +0200

    fix(dashboard-v2): always set portal domain for SkynetClient

--- packages/dashboard-v2/src/services/skynetClient.js
+++ packages/dashboard-v2/src/services/skynetClient.js
@@ -1,6 +1,3 @@
 import { SkynetClient } from "skynet-js";
 
-const { NODE_ENV, GATSBY_PORTAL_DOMAIN } = process.env;
-
-// In production-like environment, let SkynetClient figure out the best portal
-export default new SkynetClient(NODE_ENV === "development" ? `https://${GATSBY_PORTAL_DOMAIN}` : undefined);
+export default new SkynetClient(`https://${process.env.GATSBY_PORTAL_DOMAIN}`);
