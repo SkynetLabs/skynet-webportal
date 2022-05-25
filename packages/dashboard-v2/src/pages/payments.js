41466a47dab30e6cff283546a0281515348226cd
Michał Leszczyk <michasko37@gmail.com>
2022-05-25 14:05:34 +0200

    wip

--- packages/dashboard-v2/src/pages/payments.js
+++ packages/dashboard-v2/src/pages/payments.js
@@ -115,7 +115,7 @@ const PlansSlider = () => {
   const manageSubscription = async () => {
     try {
       const response = await accountsService.post("stripe/billing", { throwHttpErrors: false, redirect: "manual" });
-      console.log(response);
+      console.log(response.headers.get('location'));
       debugger;
     } catch (error) {
       setShowPaymentError(true);
