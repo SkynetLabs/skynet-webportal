79bae26fcf46f29d39d26fbbb900871d37551866
Karol Wypchlo <kwypchlo@gmail.com>
2022-06-07 16:27:04 +0200

    fix scripts/portal-down.sh

--- scripts/portal-down.sh
+++ scripts/portal-down.sh
@@ -20,7 +20,7 @@ fi
 
 countdown() {
     local secs=$1
-    while [ $secs -gt 0 ]; do
+    while [ "$secs" -gt 0 ]; do
         echo -ne "Waiting $secs\033[0K\r"
         sleep 1
         : $((secs--))
@@ -28,10 +28,10 @@ countdown() {
 }
 
 # delay disabling the portal
-countdown $delay
+countdown "$delay"
 
 # stop health-check so the server is taken our of load balancer
-docker exec health-check cli disable $reason
+docker exec health-check cli disable "$reason"
 
 # then wait 5 minutes for the load balancer to propagate the dns records
-countdown $timeout
+countdown "$timeout"
